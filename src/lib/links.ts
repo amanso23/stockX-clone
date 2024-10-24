fdcbfab7423336af011e34893b21e3d989b366b7
Adrian <adrianmanso80@gmail.com>
2024-10-24 14:57:00 +0100

    Refactor import paths for components in HeroSection, NewAndNoteworthy, PopularBrands, RecommendedSection, SectionContainer, Card, Separator, HolidaysGiftGuides, FeaturedAccessories, and BentoInfo components

---
+++ src/lib/links.ts
@@ -0,0 +1,313 @@
+export const LINKS = [
+    {
+        title: "About",
+        links: [
+            {
+                label: "How It Works",
+                href: "https://stockx.com/how-it-works"
+            },
+            {
+                label: "Verification",
+                href: "https://stockx.com/verification"
+            },
+            {
+                label: "Newsroom",
+                href: "#"
+            },
+            {
+                label: "Company",
+                href: "#"
+            },
+            {
+                label: "Careers",
+                href: "#"
+            },
+            {
+                label: "StockX Reviews",
+                href: "#"
+            },
+            {
+                label: "Give $10, Get $10",
+                href: "#"
+            }
+        ]
+    },
+    {
+        title: "Help",
+        links: [
+            {
+                label: "Help Center",
+                href: "#"
+            },
+            {
+                label: "Contact Us",
+                href: "#"
+            },
+            {
+                label: "Product Suggestions",
+                href: "#"
+            },
+            {
+                label: "Size Guide",
+                href: "#"
+            }
+        ]
+    },
+    {
+        title: "Sell",
+        links: [
+            {
+                label: "Selling Guide",
+                href: "#"
+            },
+            {
+                label: "Professional Tools",
+                href: "#"
+            },
+            {
+                label: "StockX Pro",
+                href: "#"
+            },
+            {
+                label: "Developers",
+                href: "#"
+            }
+        ]
+    },
+    {
+        title: "Air Jordan",
+        links: [
+            {
+                label: "Air Jordan 1",
+                href: "#"
+            },
+            {
+                label: "Air Jordan 3",
+                href: "#"
+            },
+            {
+                label: "Air Jordan 4",
+                href: "#"
+            },
+            {
+                label: "Air Jordan 11",
+                href: "#"
+            },
+            {
+                label: "Women's Jordan Shoes",
+                href: "#"
+            },
+            {
+                label: "Air Jordan 5",
+                href: "#"
+            }
+        ]
+    },
+    {
+        title: "Adidas",
+        links: [
+            {
+                label: "Adidas Samba",
+                href: "#"
+            },
+            {
+                label: "Adidas Campus",
+                href: "#"
+            },
+            {
+                label: "Adidas Gazelle",
+                href: "#"
+            },
+            {
+                label: "Adidas Handball Spezial",
+                href: "#"
+            },
+            {
+                label: "Adidas AE",
+                href: "#"
+            },
+            {
+                label: "Adidas Yeezy",
+                href: "#"
+            },
+        ]
+    },
+    {
+        title: "New Balance",
+        links: [
+            {
+                label: "New Balance",
+                href: "#"
+            },
+            {
+                label: "New Balance 2002R",
+                href: "#"
+            },
+            {
+                label: "New Balance 1906R",
+                href: "#"
+            },
+            {
+                label: "New Balance 530",
+                href: "#"
+            },
+            {
+                label: "New Balance 550",
+                href: "#"
+            },
+            {
+                label: "New Balance 9060",
+                href: "#"
+            },
+            {
+                label: "New Balance 990 V1",
+                href: "#"
+            }
+        ]
+    },
+    {
+        title: "Nike",
+        links: [
+            {
+                label: "Nike Air Force 1",
+                href: "#"
+            },
+            {
+                label: "Nike Air Max 1",
+                href: "#"
+            },
+            {
+                label: "Nike Dunk",
+                href: "#"
+            },
+            {
+                label: "Nike SB",
+                href: "#"
+            },
+            {
+                label: "Women's Nike Dunks",
+                href: "#"
+            },
+            {
+                label: "Nike Vomero 5",
+                href: "#"
+            }
+        ]
+    },
+    {
+        title: "Performance running",
+        links: [
+            {
+                label: "Nike Air Zoom Alphafly Next %",
+                href: "#"
+            },
+            {
+                label: "Nike ZoomX Invincible",
+                href: "#"
+            },
+            {
+                label: "Nike Pegasus 40",
+                href: "#"
+            },
+            {
+                label: "Nike Pegasus 41",
+                href: "#"
+            },
+            {
+                label: "Nike ZoomX Vaporfly Next%",
+                href: "#"
+            },
+            {
+                label: "On Cloudmonster",
+                href: "#"
+            }
+        ]
+    },
+    {
+        title: "Popular Releases",
+        links: [
+            {
+                label: "AJ1 Retro Low OG SP Travis Scott Canary (Women's)",
+                href: "#"
+            },
+            {
+                label: "Jordan 4 Retro White Thunder",
+                href: "#"
+            },
+            {
+                label: "Jordan 4 Retro SE Paris Olympics Wet Cement",
+                href: "#"
+            },
+            {
+                label: "adidas Yeezy Slide Slate Grey",
+                href: "#"
+            },
+            {
+                label: "ASICS Gel-1130 White Clay Canyon",
+                href: "#"
+            },
+            {
+                label: "Nike Kobe Mamba Mentality Jersey",
+                href: "#"
+            }
+        ]
+    },
+    {
+        title: "Apparel",
+        links: [
+            {
+                label: "Denim Tears",
+                href: "#"
+            },
+            {
+                label: "Fear of God Essentials",
+                href: "#"
+            },
+            {
+                label: "Nike Apparel",
+                href: "#"
+            },
+            {
+                label: "Supreme",
+                href: "#"
+            },
+            {
+                label: "Travis Scott",
+                href: "#"
+            },
+            {
+                label: "Yeezy",
+                href: "#"
+            }
+        ]
+    },
+    {
+        title: "Accessories",
+        links: [
+            {
+                label: "Swatch X Omega",
+                href: "#"
+            },
+            {
+                label: "Stanley",
+                href: "#"
+            },
+            {
+                label: "Designer Sunglasses",
+                href: "#"
+            },
+            {
+                label: "Louis Vuitton Accessories",
+                href: "#"
+            },
+            {
+                label: "Gucci Accessories",
+                href: "#"
+            },
+            {
+                label: "Supreme Accessories",
+                href: "#"
+            }
+        ]
+    }
+];
